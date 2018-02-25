065531dc97e1ccab379d4e85ec7a71f22bb4feb3
Dina Essam <dina.essam.fcis@gmail.com>
2018-02-25 20:29:54 +0200

    connect c# with sql

---
+++ Systel_Task1/Connection.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Configuration;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Systel_Task1
+{
+   public static class Connection
+    {
+        public static string ConVal(string name)
+        {
+            return ConfigurationManager.ConnectionStrings[name].ConnectionString;
+        }
+    }
+}
---
+++ Systel_Task1/Users.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Systel_Task1
+{
+    class Users
+    {
+        int userid { get; set; }
+        string username { get; set; }
+        string password { get; set; }
+        int privilege { get; set; }
+        int groupid { get; set; }
+    }
+}
